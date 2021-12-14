8c7da39eded1db26680c631db369e5579c9aeff6
Wojciech Wojciechowski <w.wojciechowski@nobiles2.pl>
2021-12-14 14:39:45 +0100

    testing asyncio

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from utime import sleep_ms
+import webserver.webserver as webserver
+import uasyncio
 
 def print_humidity():
   lcd.putstr("Wilgotnosc\n   %s" % bme.humidity)
@@ -9,9 +10,24 @@ def print_pressure():
 def print_temperature():
   lcd.putstr("Temperatura\n    %s" % bme.temperature)
 
-while True:
-  for func in (print_humidity, print_pressure, print_temperature):
-    lcd.clear()
-    lcd.move_to(0, 0)
-    func()
-    sleep_ms(10000)
+async def handle_webserver():
+  await webserver.run(bme)
+
+async def handle_lcd():
+  while True:
+    for func in (print_humidity, print_pressure, print_temperature):
+      lcd.clear()
+      lcd.move_to(0, 0)
+      func()
+      uasyncio.sleep(10)
+
+
+async def main():
+  # await uasyncio.create_task(handle_webserver())
+  # await uasyncio.create_task(handle_lcd())
+  loop = uasyncio.get_event_loop()
+  loop.create_task(handle_webserver())
+  loop.create_task(handle_lcd())
+  loop.run_forever()
+
+
